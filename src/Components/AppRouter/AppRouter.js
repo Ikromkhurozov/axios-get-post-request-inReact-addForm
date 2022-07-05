efc152cffe9652888755ed9b347c379f9361c57b
UmarovIkrom <ikromme13@gmail.com>
2022-07-05 19:59:26 +0500

    ...

--- src/Components/AppRouter/AppRouter.js
+++ src/Components/AppRouter/AppRouter.js
@@ -1,28 +1,3 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Nav from '../Nav/Nav'
-import PostsList from '../Posts/PostsList';
-import CommentsList from '../Comments/CommentsList';
-import AlbumsList from '../Albums/AlbumsList';
-
-
-function AppRouter() {
-    return (
-        <Router>
-            <div>
-                <Nav/>
-
-            </div>
-            <Routes>
-                <Route path='/' element={<PostsList/>} ></Route>
-                <Route path='comments' element={<CommentsList/>} ></Route>
-                <Route path='albums' element={<AlbumsList/>} ></Route>
-            </Routes>
-        </Router>
-    )
-}
-
-export default AppRouter;
 
 
 
